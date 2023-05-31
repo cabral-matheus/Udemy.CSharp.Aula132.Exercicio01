a88e92ccbdfb4bc62e151a9f5bd07cb95a4c3ed7
Matheus Cabral <matheuscabraldesousa@gmail.com>
2023-05-31 19:47:28 -0300

    class Client.cs criada

---
+++ Entities/Client.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Udemy.CSharp.Aula132.Exercicio01.Entities
+{
+    internal class Client
+    {
+    }
+}
