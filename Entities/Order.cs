5e047c2871833e2d83a3adfb4e07c81294c665d8
Matheus Cabral <matheuscabraldesousa@gmail.com>
2023-05-31 20:07:01 -0300

    class Order.cs criada

---
+++ Entities/Order.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Udemy.CSharp.Aula132.Exercicio01.Enums;
+
+namespace Udemy.CSharp.Aula132.Exercicio01.Entities
+{
+    internal class Order
+    {
+
+        public DateTime Moment { get; set; }
+        public OrderStatus Status { get; set; }
+        public Client Client { get; set; }
+        public List<OrderItem> Items { get; set; } = new List<OrderItem>();
+
+        public Order() { }
+
+        public Order(DateTime moment, OrderStatus status, Client client)
+        {
+            Moment = moment;
+            Status = status;
+            Client = client;
+        }
+
+        public void AddItem(OrderItem item)
+        {
+            Items.Add(item);
+        }
+
+        public void RemoveItem(OrderItem item)
+        {
+            Items.Remove(item);
+        }
+
+        public double Total() 
+        {
+            double total = 0;
+            foreach (OrderItem item in Items)
+            {
+                total += item.subTotal();
+            }
+
+            return total;
+        }
+    }
+}
